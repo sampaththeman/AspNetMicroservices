06fa063bc01fd0ed03ae5378e36533a88b2d39d5
Sampath Dissanayake <testsampathantler@outlook.com>
2022-10-27 22:07:58 +0530

    Mongo DB Implementations

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContex
+    {
+        public CatalogContext(IConfiguration Configuration)
+        {
+            var client = new MongoClient(Configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(Configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(Configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> ProductCollection)
+        {
+            bool exitProduct = ProductCollection.Find(p => true).Any();
+
+            if (!exitProduct)
+            {
+                ProductCollection.InsertManyAsync(GetPreConfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreConfiguredProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
